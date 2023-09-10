5f8fb012aae843b2e02bedc6771bf71b8c9da691
bahouyassine <bahouyassine7@gmail.com>
2023-09-10 23:20:50 +0200

    simple test : Create the DataFrame

--- src/Create_df.py
+++ src/Create_df.py
@@ -1,7 +1,7 @@
 from EbayScraper import EbayScraper
 
 scraper = EbayScraper()
-search_term = "PC with Ryzen 5 7600X"
+search_term = "PC with Ryzen 5 7600X and RTX 4060Ti. It has 16 GB RAM and a 1TB SSD."
 num_pages = 2
 items = scraper._get_items(f"{scraper.base_url}?_nkw={search_term.replace(' ', '+')}&_pgn={num_pages}")
 df = scraper.dataFrame
